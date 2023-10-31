abcc0832ebf1dab580e258b239e7c5e3719087f6
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-10-31 07:02:29 +0530

    Added a function which does call by value

--- constant-data-type.cpp
+++ constant-data-type.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+void print_name(const string &name) { cout << name << "\n"; }
+
 int main() {
   const int x = 42;
   const string name = "John";
   cout << "Hi, my name is " << name << " \nI am " << x << " years old \n";
+  print_name(name);
 }
