8a68e3cd04a5f28e7d2ae384d3e0fbed2547649f
Mukilan Krishnakumar <mukilan.git@gmail.com>
2023-12-04 21:03:27 +0530

    Some useful comments

--- cplusplus-tutorial/basics/preprocessor-constants.cpp
+++ cplusplus-tutorial/basics/preprocessor-constants.cpp
@@ -1,4 +1,7 @@
 // clang++ --std=c++17 --debug preprocessor-constants.cpp -o a.out
+// Basically, we don't store PI value in memory
+// It might be used a lot, this preprocessor directive saves us from lot of IO
+// operations It first processos this directive. This is also called a Macro
 #include <iostream>
 using namespace std;
 
